769f7a8f82cfdb663a70dde1b8ef7319442f03f9
Samin Yaser <saminyaserwork@gmail.com>
2023-03-31 08:05:44 +0600

    added reactive registration form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+    name = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ])
+
+    email = new FormControl('', [
+      Validators.required,
+      Validators.email
+    ])
+
+    age = new FormControl('', [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120)
+    ])
+
+    password = new FormControl('', [
+      Validators.required,
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+    ])
+
+    confirm_password = new FormControl('', [
+      Validators.required,
+    ])
+
+    phoneNumber = new FormControl('', [
+      Validators.required,
+    ])
+
+    showAlert = false;
+    alertMsg = 'Submitting... Please wait...'
+    alertColor= 'blue';
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+  });
+
+  register() {
+    this.showAlert = true;
+    this.alertMsg = 'Submitting... Please wait...'
+    this.alertColor= 'blue';
+  }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  safelist: ["bg-red-500", "bg-green-500", "bg-blue-500"],
   theme: {
     extend: {},
   },
